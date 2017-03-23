ab1de3c5f968e5b95e205ce638ef6aca02311444
Yui Wei <tyuiwei@gmail.com>
2017-03-24 03:16:13 +0800

    short script to get the points from the original calibration images

---
+++ python/getOrigPoints.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import sys
+from detectPattern import DetectContours
+
+number_points = 18
+dco = DetectContours()
+points = []
+
+for i in range(0,number_points):
+    #if i < 10:
+    #    i = str(0) + str(i)
+    img = cv2.imread("images/" + sys.argv[1] + "/" + str(i) + ".jpg")
+    a = dco.getContours(img)
+    #dci.get_circles(img)
+    points.append(dco.getCentroids(a))
+
+print points
