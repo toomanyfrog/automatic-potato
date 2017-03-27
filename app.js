e517f14c71b6986efcc7645b9095a1af2da09701
Yui Wei <tyuiwei@gmail.com>
2017-03-27 17:21:01 +0800

    web re-init idk

---
+++ app.js
@@ -0,0 +1,22 @@
+var express =   require("express");
+var bodyParser =    require("body-parser");
+var app =   express();
+var warpMedia = require('./routes/warpMedia');
+var calibrationImg = require('./routes/calibrationImg.js');
+
+app.use(bodyParser.json());
+app.use( express.static( __dirname + '/semantic' ) );
+app.use( express.static( __dirname + '/views' ) );
+app.use('/warpMedia', warpMedia);
+app.use('/calibrationImg', calibrationImg);
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
---
+++ routes/warpMedia.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var router = express.Router()
+var multer  =   require('multer');
+
+var storage =   multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+
+var upload = multer({ storage : storage }).array('userPhoto',2);
+
+router.post('/upload', function(req,res){
+    upload(req, res, function(err) {
+        console.log(req.body);
+        console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
+module.exports = router
